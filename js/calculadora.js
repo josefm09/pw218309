827b88d4948f086f12b0168e5cf5b3924f4c0b84
LOPEZ CUEN SEBASTIAN <12170621@itculiacan.edu.mx>
2018-11-06 09:46:05 -0700

    Calculadora version profe

--- js/calculadora.js
+++ js/calculadora.js
@@ -2,6 +2,7 @@ window.onload = function(){
 	var operador = "";
 	var operando1 = document.getElementById("operando1");
 	var operando2 = document.getElementById("operando2");
+	var resultado = document.getElementById("resultado");
 	var numeros = function(){
 		var valor = this.value;
 		if (operador == "") { //Operando1
@@ -47,13 +48,43 @@ window.onload = function(){
 	var nueve = document.getElementById("nueve");
 	nueve.addEventListener("click",numeros);
 
+	var suma = document.getElementById("suma");
+	var resta = document.getElementById("resta");
+	var multiplicacion = document.getElementById("multiplicacion");
+	var division = document.getElementById("division");
+	var igual = document.getElementById("igual");
+	var reiniciar = document.getElementById("reiniciar");
+
+	suma.addEventListener("click",operadores);
+	resta.addEventListener("click",operadores);
+	multiplicacion.addEventListener("click",operadores);
+	division.addEventListener("click",operadores);
+
+	igual.addEventListener("click",resultadoIgual);
+	reiniciar.addEventListener("click",reinicio);
+
 	var colorAmarillo = function(){
 		this.style.background = "yellow";
 	}
 	var colorBlanco = function(){
 		this.style.background = "white";
 	}
+	var operadores = function(){
+		operador = this.value;
+	}
+	var resultadoIgual = function(){
+		
+	}
+	var reinicio = function(){
+		operando1.value = "0";
+		operando2 = "0";
+		resultado = "0";
+		operador = "";
+	}
 
 	operando1.addEventListener("focus",colorAmarillo);
 	operando1.addEventListener("focusout",colorBlanco);	
+	operando2.addEventListener("focus",colorAmarillo);
+	operando2.addEventListener("focusout",colorBlanco);	
+
 }
